2723fa9b13d0924b92db762d1658e9d5d0dc03eb
Margaret Njenga <maggienjenga57@gmail.com>
2025-05-08 10:48:53 +0100

    Add project files.

---
+++ Assessment/LinkedList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace TextAnalysisTool
+{
+    internal class LinkedList
+    {
+        private Node headlist  = null; //initialize an empty list that points to the first node in the list
+        public void AddUnique(string word) //add a word to the front of the list
+        {
+            Node temp = headlist;
+            while (temp != null)
+            {
+                if (temp.Data == word)
+                {
+                    temp.Count++; //if word already exists increase count
+                    return;
+                }
+                temp = temp.Next;
+            }
+
+            Node newNode = new Node(word); //create a new node
+            newNode.Next = headlist; // link new node to the current head
+            headlist = newNode; //Move head to point to new node          
+        }
+        public int CountUniqueWords()
+        {
+            int UniqueCount = 0;
+            Node temp = headlist;
+            while (temp != null)
+            {
+                UniqueCount++;
+                temp = temp.Next;
+            }
+            return UniqueCount;
+        }
+        public void Display()
+        {
+            Node temp = headlist;
+            while (temp != null)
+            {
+               Console.WriteLine($"{temp.Data}: {temp.Count} times");
+               temp = temp.Next;
+            }
+        }
+
+        public void DisplaySorted()
+        {
+            List<(string word, int count)> sortedList = new List<(string, int)>(); //create new temporary list to store all the words and their counts
+            Node temp = headlist;
+
+            while (temp != null)
+            {
+                sortedList.Add((temp.Data, temp.Count)); //at each step, add the current node's word and count into the new list
+                temp = temp.Next; 
+            }
+
+            var sorted = sortedList.OrderBy(w => w.word);
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine($"{item.word}:{item.count} times");
+            }
+        }
+
+        public(string word, int count) GetLongestWord()
+        {
+            Node temp = headlist;
+            string longestword = "";
+            int count = 0;
+
+            while (temp != null)
+            {
+                if (temp.Data.Length > longestword.Length)
+                {
+                    longestword = temp.Data;
+                    count = temp.Count;
+                }
+                temp = temp.Next;
+            }
+            return(longestword, count);
+        }
+
+        public(string word, int count) GetMostFrequentWord()
+        {
+            Node temp = headlist;
+            string mostFrequent = "";
+            int highestcount = 0;
+
+            while (temp != null)
+            {
+                if (temp.Count > highestcount)
+                {
+                    mostFrequent = temp.Data;
+                    highestcount = temp.Count;
+                }
+                temp = temp.Next;
+            }
+            return (mostFrequent, highestcount);
+        }
+
+        public void ShowWordFrequency(string word)
+        {
+            string input = word.ToLower();
+            Node temp = headlist;
+            while (temp != null)
+            {
+                if(temp.Data == input)
+                {
+                    Console.WriteLine($"The word '{word}' appears {temp.Count} times.");
+                    return;
+                }
+                temp = temp.Next;
+            }
+            Console.WriteLine($"The word '{word}' was not found in the file.");
+        }
+
+
+
+
+    }
+}
---
+++ Assessment/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysisTool
+{
+    internal class Node // we first create a blueprint for each node
+    {
+        private string data; //field to store the data of the node which is some word in a text
+        private int count; //field to store the number of times the word appears in a node
+        private Node next; //field to store the reference to the next node in the list
+
+        public Node(string word)
+        {
+            this.data = word; //initialize the data field with the provided word
+            this.count = 1; //start counting from 1 when each word is created
+            this.next = null; //initialize the next field to null as the new node does not point to anything yet
+        }
+        public Node(string word, Node list)
+        {
+            this.data = word;
+            this.count = count;
+            this.next = list;
+        }
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+        public int Count
+        {
+            get { return this.count; }
+            set { this.count = value; }
+        }
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
